1e19b226eb5351c686349413dc8f77dad61e87b4
Savchuk <savchuk-sergej@smartworld.team>
2020-09-26 10:13:16 +0700

    add custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Error 404</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+name: "error"
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
