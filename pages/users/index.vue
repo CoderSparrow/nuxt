2b7dd852b4c1bde6190e6e5097679efd74c66911
Savchuk <savchuk-sergej@smartworld.team>
2020-09-26 09:49:08 +0700

    add dynamic routing and routing validation

---
+++ pages/users/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h1>User page</h1>
+    <ul>
+      <li v-for="user in users">
+        <a href="#" @click.prevent="goTo(user)">User {{user}}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "users",
+  data: () => ({
+    users: [
+      1, 2, 3, 4, 5
+    ]
+  }),
+  methods: {
+    goTo (userId) {
+      this.$router.push('/users/' + userId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
